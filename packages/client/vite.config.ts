6aa10979530c1c0c5baa067b8cd5828fa1b1cc7f
mohamad <mohamad@gmail.com>
2025-11-01 13:32:12 +0330

    commit

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "#base": path.resolve(__dirname, "../base/src"),
-      "#components": path.resolve(__dirname, "../ui/src/components"),
-      "#ui": path.resolve(__dirname, "../ui/src"),
-      "#shared": path.resolve(__dirname, "../shared/src"),
+      "#base": path.resolve(__dirname, "../core/packages/base/src"),
+      "#components": path.resolve(
+        __dirname,
+        "../core/packages/ui/src/components"
+      ),
+      "#ui": path.resolve(__dirname, "../core/packages/ui/src"),
+      "#shared": path.resolve(__dirname, "../core/packages/shared/src"),
       "#client": path.resolve(__dirname, "./src"),
     },
   },
